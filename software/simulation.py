c3d2f3efcd88a1bd2f4d159a677aab2e1bbe1586
arcturus <arcturus@protonmail.com>
2024-12-11 20:37:21 +0200

    restructured simulation

--- software/simulation.py
+++ software/simulation.py
@@ -1,25 +1,13 @@
-from database import Database, Portfolio
+from database import Database
 from security import credentials
-
 
 class Simulations:
     
     def __init__(self):
         """Set up the class for simulating portfolio performance."""
-        self.db = credentials.DB
-        self.user = credentials.USER
-        self.password = credentials.PASSWORD
-        self.host = credentials.HOST
-        self.port = credentials.PORT
-        self.pg_exe = credentials.PG_EXE_PATH
-        
-        self.portfolio = self.collect_portfolio_data()
-        
-    def collect_portfolio_data(self):
-        """Collect the data from the portfolio table."""
-        with Database(db=self.db, user=self.user, password=self.password, host=self.host, port=self.port, pg_exe=self.pg_exe) as db:
-            portfolio_data = db.fetch_data('PORTFOLIO')    
-        return portfolio_data
+        with Database(db=credentials.DB, user=credentials.USER, password=credentials.PASSWORD, host=credentials.HOST, port=credentials.PORT, pg_exe=credentials.PG_EXE_PATH) as self.db:
+            data = self.db.fetch_data('PORTFOLIO')
+            print(data)
     
 Simulations()
 
