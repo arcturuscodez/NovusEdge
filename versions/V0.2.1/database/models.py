32aeb2c173a48dbe61bd9747a91ecbb0e25b6db0
arcturus <arcturus@protonmail.com>
2025-01-19 14:51:46 +0200

    add comments to models

--- versions/V0.2.1/database/models.py
+++ versions/V0.2.1/database/models.py
@@ -47,26 +47,26 @@ class ShareholderModel(BaseModel):
     """
     Dataclass model to represent a shareholder.
     """ 
-    id: Optional[int] = None
-    name: str = ""
-    ownership: float = 0.0 
-    investment: float = 0.0
-    email: str = ""
-    shareholder_status: Optional[str] = 'active'
-    created_at: Optional[datetime] = field(default_factory=datetime.now)
+    id: Optional[int] = None # Shareholder ID
+    name: str = "" # Shareholder name
+    ownership: float = 0.0 # Shareholder ownership percentage of the firm
+    investment: float = 0.0 # Shareholder investment in the firm
+    email: str = "" # Shareholder email
+    shareholder_status: Optional[str] = 'active' # Shareholder status
+    created_at: Optional[datetime] = field(default_factory=datetime.now) # Shareholder creation date
     
 @dataclass
 class TransactionModel(BaseModel):
     """ 
     Dataclass model to represent a transaction.
     """
-    id: Optional[int] = None
-    ticker: str = ""
-    shares: float = 0.0
-    price_per_share: float = 0.0
-    total_value: Optional[float] = None
-    transaction_type: str = ""
-    created_at: Optional[datetime] = field(default_factory=datetime.now)
+    id: Optional[int] = None # Transaction ID
+    ticker: str = "" # Asset ticker symbol
+    shares: float = 0.0 # Number of shares or number of asset purchased
+    price_per_share: float = 0.0 # Price per share of the asset
+    total_value: Optional[float] = None # Total value of the transaction
+    transaction_type: str = "" # Type of transaction (buy, sell, etc.)
+    created_at: Optional[datetime] = field(default_factory=datetime.now) # Transaction creation date
 
 @dataclass
 class PortfolioModel(BaseModel):
