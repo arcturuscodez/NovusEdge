0c50a66f0ac615a74ae3bab3c64c37ec9d78acf1
Arcturus <sh_main@outlook.com>
2025-03-05 12:47:46 +0200

    update database init

--- software/database/__init__.py
+++ software/database/__init__.py
@@ -1,6 +1,13 @@
+"""
+The database package provides modules for managing database connections,
+repositories, and models related to shareholders and other entities.
+"""
 from .connection import DatabaseConnection
 
-from .models import BaseModel, GenericModel, ShareholderModel, TransactionModel, PortfolioModel, FirmModel, TaskModel
+from .models import (
+    BaseModel, GenericModel, ShareholderModel, 
+    TransactionModel, PortfolioModel, FirmModel, TaskModel
+)
 
 from .repositories.shareholder import ShareholderRepository
 from .repositories.transaction import TransactionRepository
@@ -8,12 +15,7 @@ from .repositories.portfolio import PortfolioRepository
 from .repositories.firm import FirmRepository
 from .repositories.task import TaskRepository
 
-"""
-The database package provides modules for managing database connections,
-repositories, and models related to shareholders and other entities.
-"""
-
-__version__ = "0.2.5"
+__version__ = "0.3.0"
 __author__ = "Sonny Holman"
 
 __all__ = [
